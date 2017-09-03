90b42888b9cb5ae45d70b22ae27b0cfeeb0dd317
Michael Yu <addyufei@163.com>
2017-09-03 19:45:59 +0800

    add somethings

---
+++ python_lib/string/basic.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = 'Michael Yu'
+import string
+
+"""
+常用的字符串常量方法
+"""
+print string.digits
+print string.letters
+print string.lowercase
+print string.printable
+print string.punctuation
+print string.uppercase
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
